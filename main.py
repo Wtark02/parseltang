55ad73101a3b5d4d5d814d698cf17b65b6ef52c7
Wtark02 <61852180+Wtark02@users.noreply.github.com>
2020-03-06 18:07:21 +0300

    Попытка 4

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_last_episode(message):
 	episode_text = text = "{}\n\n{}\n\n{}".format(title,description,mp3_url)
 	bot.send_message(message.chat.id, episode_text)
 	try:
-		audio = open('http://radiotony.ru/wp-content/uploads/2020/03/z_uk-peremotki-2.mp3', 'rb')
+		audio = open('/tmp/audio.mp3', 'rb')
 		bot.send_audio(message.chat.id, audio)
 		bot.send_audio(message.chat.id, "FILEID")
 	except Exception as e:
