1ddff8b970be13f7fe21ddb4f846b90cc87e82cd
Wtark02 <61852180+Wtark02@users.noreply.github.com>
2020-03-10 12:20:28 +0300

    Все же не работает

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def get_last_episode(message):
 	markup = types.InlineKeyboardMarkup()
 	btn_my_site = types.InlineKeyboardButton(text='Слушать выпуск', url=mp3_url_2)
 	markup.add(btn_my_site)
-	# bot.send_message(message.chat.id, "Нажми на кнопку, чтобы послушать новый выпуск", reply_markup = markup)
+	bot.send_message(message.chat.id, "Нажми на кнопку, чтобы послушать новый выпуск", reply_markup = markup)
 
 	    
 bot.polling()
