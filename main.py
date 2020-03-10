8c6b3fe0349560353ff6f4721f37fedc0d5ac7f6
Wtark02 <61852180+Wtark02@users.noreply.github.com>
2020-03-10 12:02:49 +0300

    В жопу закачку mp3, она пока не будет работать.

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ import telebot
 from config import TG_TOKEN
 
 token = TG_TOKEN
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot('1137799389:AAGF_kLE4v0jUATJPAtlcXLR-6F0P_naZo8')
 
 @bot.message_handler(commands=['start','help'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет! Я (в будущем) бот для автопарсинга новых выпусков с PODSTER.FM\n\nПришли мне сообщение вида /rss "адрес RSS"')
+    bot.send_message(message.chat.id, 'Привет! Я (в будущем) бот для автопарсинга новых выпусков\n\nПришли мне сообщение вида /rss "адрес RSS"')
 
 @bot.message_handler(commands=['rss'])
 def get_last_episode(message):
@@ -33,6 +33,11 @@ def get_last_episode(message):
 		bot.send_audio(message.chat.id, audio)
 		bot.send_audio(message.chat.id, "FILEID")
 	except Exception as e:
-        	bot.send_message(message.chat.id, mp3_url_2)
+        	@bot.message_handler(commands = ['url'])
+		def url(message):
+		    markup = types.InlineKeyboardMarkup()
+		    btn_my_site= types.InlineKeyboardButton(text='Слушать выпуск', url=mp3_url_2)
+		    markup.add(btn_my_site)
+		    bot.send_message(message.chat.id, "Нажми на кнопку, чтобы послушать новый выпуск", reply_markup = markup)
 
 bot.polling()
